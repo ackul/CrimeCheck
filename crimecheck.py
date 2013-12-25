93d1a1d2f052c776a4de7b7d48f764fdfc6a8fce
Achin Kulshrestha <achinkul@gmail.com>
2013-12-25 17:54:56 +0530

    Optimized PCAP Parsing, added sample.pcap file

--- crimecheck.py
+++ crimecheck.py
@@ -13,12 +13,17 @@ import urllib2
 import logging, getopt
 import os
 import sys
+import socket
+p = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'third_party', 'dpkt')
+if p not in sys.path:    
+    sys.path.insert(0, p)
 import dpkt
+from collections import defaultdict
 
 DEBUG = False
 capture=""
 def sendRequest(serverAddr):
-    req = urllib2.Request(serverAddr)
+    req = urllib2.Request("https://"+serverAddr)
     req.add_header('Accept-Encoding', 'gzip,deflate')
     resp = urllib2.urlopen(req)
     content = resp.read()
@@ -26,9 +31,9 @@ def sendRequest(serverAddr):
 
 def checkIfVulnerable(respCode,serverAddr):
     if respCode:
-        logger.info("Server %s supports HTTP Compression",serverAddr)
+        logger.info("HTTP Compression for Server %s is ENABLED ",serverAddr)
     else:
-        logger.info("Server %s doesn't support HTTP Compression",serverAddr)
+        logger.info("HTTP Compression for Server %s is DISABLED",serverAddr)
 def pcap_reader(fp):
     return dpkt.pcap.Reader(fp)
 
@@ -43,7 +48,7 @@ def as_percent(a, b):
 
 TLS_HANDSHAKE = 22
 
-def gather_statistics(cap):
+def gather_statistics(cap,serverAddr):
     counters = defaultdict(int)
     known_extensions = set()
     count_extensions = set()
@@ -62,19 +67,15 @@ def gather_statistics(cap):
         tcp = ip.data
         if tcp.dport != 443 and tcp.sport != 443:
             continue
-
+        if socket.gethostbyname(serverAddr) != socket.inet_ntoa(ip.src):
+            continue
+        
         if len(tcp.data) <= 0:
             continue
 
         # we only care about handshakes for now...
         if ord(tcp.data[0]) != TLS_HANDSHAKE:
             continue
-
-        if DEBUG:
-            print 'tcp.sport: %d' % (tcp.sport)
-            print 'tcp.dport: %d' % (tcp.dport)
-            print 'tcp.data[0]: %d' % ord(tcp.data[0])
-            print 'tcp.sum: 0x%x' % tcp.sum
 
         records = []
         try:
@@ -96,101 +97,43 @@ def gather_statistics(cap):
             if len(record.data) == 0:
                 continue
             # Client Hello only
-            if ord(record.data[0]) != 1:
+            if ord(record.data[0]) != 2:
                 continue
+            counters['Handshake_hellos_total'] += 1
+            if counters['Handshake_hellos_total'] == 1:
+                try:
+                    handshake = dpkt.ssl.TLSHandshake(record.data)
+                except dpkt.dpkt.NeedData, e:
+                    # TODO: shouldn't happen in practice for handshakes... but could. meh.
+                    continue         
+            
 
-            try:
-                handshake = dpkt.ssl.TLSHandshake(record.data)
-            except dpkt.dpkt.NeedData, e:
-                # TODO: shouldn't happen in practice for handshakes... but could. meh.
-                continue
+                ch = handshake.data
 
-            if not isinstance(handshake.data, dpkt.ssl.TLSClientHello):
-                continue
+                if ch.version == dpkt.ssl.SSL3_V:
+                    counters['SSLv3_clients'] += 1
+                elif ch.version == dpkt.ssl.TLS1_V:
+                    counters['TLSv1_clients'] += 1
+                elif ch.version == dpkt.ssl.TLS11_V:
+                    counters['TLSv1.1_clients'] += 1
+                elif ch.version == dpkt.ssl.TLS12_V:
+                    counters['TLSv1.2_clients'] += 1
 
-            counters['client_hellos_total'] += 1
+                if len(ch.session_id) > 0:
+                    counters['session_id_sent'] += 1
+                if ch.compression != 0:
+                    counters['TLSCompression'] += 1
 
-            ch = handshake.data
-
-            if ch.version == dpkt.ssl.SSL3_V:
-                counters['SSLv3_clients'] += 1
-            elif ch.version == dpkt.ssl.TLS1_V:
-                counters['TLSv1_clients'] += 1
-            elif ch.version == dpkt.ssl.TLS11_V:
-                counters['TLSv1.1_clients'] += 1
-            elif ch.version == dpkt.ssl.TLS12_V:
-                counters['TLSv1.2_clients'] += 1
-
-            if len(ch.session_id) > 0:
-                counters['session_id_sent'] += 1
-
-            if DEBUG:
-                print ""
-                print 'ch.session_id.version: %s' % dpkt.ssl.ssl3_versions_str[ch.version]
-                print 'ch.session_id.len: %d' % len(ch.session_id)
-                print 'ch.num_ciphersuites: %d' % ch.num_ciphersuites
-                print 'ch.num_compression_methods: %d' % ch.num_compression_methods
-                print 'ch.compression_methods: %s' % str(ch.compression_methods)
-
-            if 1 in ch.compression_methods:
-                counters['deflate_support'] += 1
-
-            counters['extension_count_%d' % (len(ch.extensions))] += 1
-            count_extensions.add(len(ch.extensions))
-
-            if DEBUG:
-                import binascii
-                for ext in ch.extensions:
-                    print 'extType: (%d) %s' % (ext.value, ext.name)
-                    print 'extData: %s' % (binascii.hexlify(ext.data))
-            for ext in ch.extensions:
-                known_extensions.add(ext.name)
-                counters['ext_%s' % (ext.name)] += 1
-    stats = [
-        {
-            'name': 'Client Hello seen',
-            'value': str(counters['client_hellos_total']),
-        },
-        {
-            'name': 'SSL v3 Clients',
-            'value': as_percent(counters['SSLv3_clients'], counters['client_hellos_total']),
-        },
-        {
-            'name': 'TLS v1 Clients',
-            'value': as_percent(counters['TLSv1_clients'], counters['client_hellos_total']),
-        },
-        {
-            'name': 'TLS v1.1 Clients',
-            'value': as_percent(counters['TLSv1.1_clients'], counters['client_hellos_total']),
-        },
-        {
-            'name': 'TLS v1.2 Clients',
-            'value': as_percent(counters['TLSv1.2_clients'], counters['client_hellos_total']),
-        },
-        {
-            'name': 'Sent SessionID',
-            'value': as_percent(counters['session_id_sent'], counters['client_hellos_total']),
-        },
-        {
-            'name': 'Deflate Support',
-            'value': as_percent(counters['deflate_support'], counters['client_hellos_total']),
-        }
-        ]
-
-    for ext in sorted(known_extensions):
-        stats.append({
-            'name': 'Support for %s extension' % ext,
-            'value': as_percent(counters['ext_%s' % ext], counters['client_hellos_total']),
-        })
-    for count in sorted(count_extensions):
-        stats.append({
-            'name': 'Sent %d extension' % count,
-            'value': as_percent(counters['extension_count_%s' % count], counters['client_hellos_total']),
-        })
-    return stats
-
-        
-
+            
+                if counters['TLSCompression'] > 0:
+                    logger.info("The TLS Compression for server %s is ENABLED", serverAddr)
+                    break
+                else:
+                    logger.info("The TLS Compression for server %s is DISABLED", serverAddr)
+                    break
+    if counters['Handshake_hellos_total'] == 0:
+        logger.info("The PCAP file doesn't contain any SSL Handshake Packets corresponding to %s ", serverAddr)              
+ 
 if __name__ == '__main__':
 
     __usage__ = __doc__.replace("PROGRAM", os.path.basename(sys.argv[0]))
@@ -211,11 +154,15 @@ if __name__ == '__main__':
         if o in ("-h","--help"): die_usage()
         if o in ("-v", "--verbose"): logger.setLevel(logging.DEBUG)                
         if o in ("-l", "--log-file"): fileHandler = logging.FileHandler(a)
-        if o in ("-s", "--server-url"):serverAddr = str(a)
+        if o in ("-s", "--server-url"):
+            serverAddr = str(a)
+            pcap = None
         if o in ("-p", "--pcap-file"):
-            with open(a, 'rb') as fp:
-                capture = pcap_reader(fp)
-                stats = gather_statistics(capture)
+            if a:
+                pcap = str(a)
+            else:
+                logger.info("Please enter a valid pcap file")
+                
           
     handler = logging.StreamHandler(sys.stdout)
     formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
@@ -224,19 +171,23 @@ if __name__ == '__main__':
     if (fileHandler is not None):
         fileHandler.setFormatter(formatter)
         logger.addHandler(fileHandler)
-    serverAddr="https://google.com"
-    logger.info("Ready. Sending Request... ")
+    
+    
     if serverAddr == "":
         die_usage("Please Enter Server Address")
-    try:
-       respCode = sendRequest(serverAddr)
-       checkIfVulnerable(respCode,serverAddr)
-       if capture:
-           stats = gather_statistics(capture)
-       else:
-           logger.info("To check for TLS Compression, Please provide a PCAP file")
-    except:
-        logger.info("Error...Booting out")
+    try:      
+        logger.info("Ready. Sending Request to server %s", serverAddr)
+        respCode = sendRequest(serverAddr)
+        checkIfVulnerable(respCode,serverAddr)
+        logger.info("Checking TLS Compression for %s", serverAddr)
+        if pcap:
+            fp = open(pcap,'rb')
+            capture = pcap_reader(fp)
+            stats = gather_statistics(capture, serverAddr)
+        else:
+            logger.info("To check for TLS Compression, Please provide a PCAP file")
+    except Exception as e :
+        logger.info("Error - %s",e)
     
 
 
